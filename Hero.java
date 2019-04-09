4f98c7de564e636d5419fd3ff2a7029f274eb100
Kevin van der Zwan <302147728@student.rocmondriaan.nl>
2019-04-09 10:21:26 +0200

    eZ

--- Hero.java
+++ Hero.java
@@ -1,117 +1,150 @@
 
-import greenfoot.*;
-import java.util.*;
-
-/**
-*
-* @author R. Springer
-*/
-public class Hero extends Mover {
-
-private final double gravity;
-private final double acc;
-private final double drag;
-
-private int frame = 1;
-private int speed = 3;
-private boolean onGround;
-public  int leven = 3;
-private int spawnX;
-private int spawnY;
-private CollisionEngine collisionEngine;
-private TileEngine tileEngine;
-private boolean isTouching;
-private int lockpick = 0;
-private int lockpick_total = 0;
-private int score = 0;
-private int status = 0;
-public int levens = 3;
-public String worldName="";
-int Gem= 0;
-public boolean eatKeys;
-boolean key = false;
-public boolean starAdded = false;
-public boolean keyBlueAdded = false;
-public boolean keyGreenAdded = false;
-public boolean keyRedAdded = false;
-public boolean gemAdded = false;
-
-private int setPlaynumber = 1;
-private int walkL = -10;
-private int walkR = 10;
-private int springNumy;
-
-private boolean toLevel2;
-int star = 0;
-
-private ArrayList<GreenfootImage> animatieP1;
-private ArrayList<GreenfootImage> animatieP2;
-private ArrayList<GreenfootImage> animatieP3;
-private ArrayList<GreenfootImage> currentAnimation;
-
-public Hero(CollisionEngine collisionEngine, TileEngine tileEngine) {
-    super();
-    this.collisionEngine = collisionEngine;
-    this.tileEngine = tileEngine;
-    gravity = 9.8;
-    acc = 0.6;
-    drag = 0.8;
-    this.worldName = worldName;
-    setImage("p1_front.png");
-    animatieP1 = new ArrayList<GreenfootImage>();
-    animatieP2 = new ArrayList<GreenfootImage>();
-    animatieP3 = new ArrayList<GreenfootImage>();
-    for(int i = 1; i <= 11; i++) {
-        String imageStart = "p1_walk0";
-        animatieP1.add(new GreenfootImage(imageStart + i + ".png"));
-        animatieP2.add(new GreenfootImage(imageStart + i + ".png"));
-        animatieP3.add(new GreenfootImage(imageStart + i + ".png"));
-    }
-    // verander current animatie naar de nieuw character zodra je deze oppakt.
-    currentAnimation = animatieP1;
-    currentAnimation = animatieP2;
-    currentAnimation = animatieP3;
-    
-}
-
-@Override
-public void act() {
-    handleInput();
-    velocityX *= drag;
-    velocityY += acc;
-    if (velocityY > gravity) {
-        velocityY = gravity;
-    }
-    applyVelocity();
-    handleInput();
-    toLevel2();
-
-  
-    
-   
-
-    for (Actor enemy : getIntersectingObjects(Enemy.class)) {
-        if (enemy != null) {
-            setLocation(300,200);
-            setPlaynumber = 1;
-            break;
+        
+        import greenfoot.*;
+        import java.util.*;
+        
+        /**
+        *
+        * @author R. Springer
+        */
+        public class Hero extends Mover {
+        
+        private final double gravity;
+        private final double acc;
+        private final double drag;
+        private String kleur = "groen";
+        private int frame = 1;
+        private int speed = 3;
+        private boolean onGround;
+        public  int leven = 3;
+        private int spawnX;
+        private int spawnY;
+        private CollisionEngine collisionEngine;
+        private TileEngine tileEngine;
+        private boolean isTouching;
+        private int lockpick = 0;
+        private int lockpick_total = 0;
+        private int score = 0;
+        private int status = 0;
+        public int levens = 3;
+        public String worldName="";
+        int Gem= 0;
+        public boolean eatKeys;
+        boolean key = false;
+        public boolean starAdded = false;
+        public boolean keyBlueAdded = false;
+        public boolean keyGreenAdded = false;
+        public boolean keyRedAdded = false;
+        public boolean gemAdded = false;
+        
+        private int setPlaynumber = 1;
+        private int walkL = -10;
+        private int walkR = 10;
+        private int springNumy;
+        
+        
+        
+        private boolean toLevel2;
+        int star = 0;
+        
+        private ArrayList<GreenfootImage> animatieP1;
+        private ArrayList<GreenfootImage> animatieP2;
+        private ArrayList<GreenfootImage> animatieP3;
+        private ArrayList<GreenfootImage> currentAnimation;
+        private static GreenfootImage[] mirror = new GreenfootImage[33];
+        public Hero(CollisionEngine collisionEngine, TileEngine tileEngine) {
+            super();
+            this.collisionEngine = collisionEngine;
+            this.tileEngine = tileEngine;
+            gravity = 9.8;
+            acc = 0.6;
+            drag = 0.8;
+            this.worldName = worldName;
+            setImage("p1_front.png");
+            animatieP1 = new ArrayList<GreenfootImage>();
+            animatieP2 = new ArrayList<GreenfootImage>();
+            animatieP3 = new ArrayList<GreenfootImage>();
+            String imageStart1 = "p1_walk0";
+            String imageStart2 = "p2_walk0";
+            String imageStart3 = "p3_walk0";
+            for(int i = 1; i <= 11; i++) {
+                animatieP1.add(new GreenfootImage(imageStart1 + i + ".png"));
+                animatieP2.add(new GreenfootImage(imageStart2 + i + ".png"));
+                animatieP3.add(new GreenfootImage(imageStart3 + i + ".png"));
+            }
+            // verander current animatie naar de nieuw character zodra je deze oppakt.
+            currentAnimation = animatieP1;
+            currentAnimation = animatieP2;
+            currentAnimation = animatieP3;
+        
+            
+            
+            
         }
-    }
-    List <Tile> tiles = collisionEngine.getCollidingTiles(this, true);
-    for (Tile tile : getIntersectingObjects(Tile.class)) {
-
-        if (tile != null) {
-
-            if(tile.getImage().toString().contains("hud_p1Alt")){
-             setPlaynumber = 1;
-            // getWorld().removeObject(tile);
-             break;
-     }else if(tile.getImage().toString().contains("hud_p2Alt")){
-             setPlaynumber = 2;
-            // getWorld().removeObject(tile);
-             break;
-     }else if(tile.getImage().toString().contains("hud_p3Alt")){
-             setPlaynumber = 3;
+        
+            
+        
+        @Override
+        public void act() {
+            handleInput();
+            velocityX *= drag;
+            velocityY += acc;
+            if (velocityY > gravity) {
+                velocityY = gravity;
+            }
+            applyVelocity();
+            handleInput();
+            toLevel2();
+        
+            for (Tile tile : getIntersectingObjects(Tile.class)) {
+                if(tile.getImage().toString().contains("hud_p1Alt")){
+                    setImage("p1.png");
+                    kleur = "groen";
+                    currentAnimation = animatieP1;
+                    animatie();
+                }
+                else if(tile.getImage().toString().contains("hud_p2Alt")){
+                    setImage("p2_front.png");
+                    kleur = "blauw";
+                    currentAnimation = animatieP2;
+                    animatie();
+                }
+                else if(tile.getImage().toString().contains("hud_p3Alt")){
+                    setImage("p3_front.png");
+                    kleur = "roze";
+                    currentAnimation = animatieP3;
+                    animatie();
+                    
+                }
+            }
+            
+           
+        
+            for (Actor enemy : getIntersectingObjects(Enemy.class)) {
+                if (enemy != null) {
+                    setLocation(300,200);
+                    setPlaynumber = 1;
+                    break;
+                }
+            }
+            List <Tile> tiles = collisionEngine.getCollidingTiles(this, true);
+            for (Tile tile : getIntersectingObjects(Tile.class)) {
+        
+                if (tile != null) {
+        
+                    if(tile.getImage().toString().contains("hud_p1Alt")){
+                     setPlaynumber = 1;
+                     currentAnimation = animatieP1;
+                    // getWorld().removeObject(tile);
+                     break;
+             }else if(tile.getImage().toString().contains("hud_p2Alt")){
+                     setPlaynumber = 2;
+                     currentAnimation = animatieP2;
+                    // getWorld().removeObject(tile);
+                     break;
+             }else if(tile.getImage().toString().contains("hud_p3Alt")){
+                 setPlaynumber = 3;
+                 currentAnimation = animatieP3;
             // getWorld().removeObject(tile);
              break;
             }
@@ -174,7 +207,8 @@ public void act() {
                  
                 }
                 if (tile.type == TileType.KEYRED) {
-                        tileEngine.removeTile(tile);
+                        tileEngine.removeTile(tile 
+                        );
                         this.keyRedAdded = true;
                         for(Door door : getWorld().getObjects(Door.class) ) {
                             if(door.type == TileType.REDLOCK) {
@@ -244,7 +278,7 @@ public void act() {
         }
     if (Greenfoot.isKeyDown("w") && (onGround() == true)) {
         velocityY = -15;
-        setImage("p1_jump.png");
+        setImage("p" + setPlaynumber + "_jump.png");
     }
 
     else if (Greenfoot.isKeyDown("a")) {
@@ -312,7 +346,7 @@ public void toLevel2() {
 
     public void animatie() {
         frame++;
-        if(frame >= 11) frame = 0;
+        if(frame >= 11){ frame = 0;}
         setImage(currentAnimation.get(frame));
    }
    
